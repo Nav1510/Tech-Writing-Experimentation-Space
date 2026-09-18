48fc56c2500d76cc25541ec82430004b700cad0b
Naveen_M <147331755+Nav1510@users.noreply.github.com>
2026-09-18 15:14:30 +0530

    Index (comps)

---
+++ DITA - Learnings/Sample User Guide/out/temp/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,32 @@
+var keywords=[{w:"Close",p:["p0"]},{w:"Active",p:["p0","p3","p23"]},{w:"Jobs",p:["p0","p3","p4","p10","p11","p14"]},{w:"Create",p:["p1"]},{w:"and",p:["p1","p4"]},{w:"Publish",p:["p1","p4"]},{w:"Job",p:["p1","p13","p19","p23","p25"]},{w:"Context",p:["p2"]},{w:"Sensitive",p:["p2"]},{w:"Help",p:["p2"]},{w:"Edit",p:["p3","p4"]},{w:"Modified",p:["p4"]},{w:"Find",p:["p5","p16"]},{w:"Talent",p:["p5","p16"]},{w:"For",p:["p6","p7","p8","p9","p10"]},{w:"Agencies",p:["p6"]},{w:"Agency",p:["p7"]},{w:"Crowd",p:["p8"]},{w:"(Private",p:["p8"]},{w:"Jobs)",p:["p8"]},{w:"Professionals",p:["p9"]},{w:"Invite",p:["p10","p18"]},{w:"Vendor",p:["p10","p14"]},{w:"Resources",p:["p10","p14","p17","p18"]},{w:"from",p:["p10"]},{w:"Resource",p:["p10"]},{w:"Management",p:["p10"]},{w:"Manage",p:["p11"]},{w:"Overview",p:["p12"]},{w:"Place",p:["p13"]},{w:"on",p:["p13"]},{w:"Hold",p:["p13"]},{w:"Post",p:["p14"]},{w:"to",p:["p14","p18"]},{w:"Prerequisites",p:["p15"]},{w:"Sections",p:["p16"]},{w:"Under",p:["p16"]},{w:"Select",p:["p17"]},{w:"Send",p:["p18"]},{w:"Email",p:["p18"]},{w:"Selected",p:["p18"]},{w:"Step",p:["p19","p20","p21"]},{w:"1:",p:["p19"]},{w:"Information",p:["p19"]},{w:"2:",p:["p20"]},{w:"Sourcing",p:["p20"]},{w:"Requirement(s)",p:["p20"]},{w:"3:",p:["p21"]},{w:"Project",p:["p21"]},{w:"Documents",p:["p21","p24"]},{w:"User",p:["p22"]},{w:"Roles",p:["p22"]},{w:"View",p:["p23","p24","p25"]},{w:"Details",p:["p23"]},{w:"Pending",p:["p25"]},{w:"Invites",p:["p25"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5, 6];
+ph["p2"]=[7, 8, 9];
+ph["p3"]=[10, 1, 2];
+ph["p4"]=[10, 4, 5, 11, 2];
+ph["p5"]=[12, 13];
+ph["p6"]=[14, 15];
+ph["p7"]=[14, 16];
+ph["p8"]=[14, 17, 18, 19];
+ph["p9"]=[14, 20];
+ph["p10"]=[21, 22, 23, 14, 2, 24, 25, 26];
+ph["p12"]=[28];
+ph["p11"]=[27, 2];
+ph["p14"]=[32, 2, 33, 22, 23];
+ph["p13"]=[29, 6, 30, 31];
+ph["p16"]=[35, 36, 12, 13];
+ph["p15"]=[34];
+ph["p18"]=[38, 39, 21, 33, 40, 23];
+ph["p17"]=[37, 23];
+ph["p19"]=[41, 42, 6, 43];
+ph["p21"]=[41, 47, 48, 49];
+ph["p20"]=[41, 44, 45, 46];
+ph["p23"]=[52, 1, 6, 53];
+ph["p22"]=[50, 51];
+ph["p25"]=[52, 54, 6, 55];
+ph["p24"]=[52, 49];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ DITA - Learnings/Sample User Guide/out/temp/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {3:12,14:3,19:5,8:11,9:15,15:-1,21:1,6:15,11:-1,20:1,24:12,5:-1,10:11,0:12,16:-1,18:5,7:11,25:24,23:-1,13:-1,1:-1,12:-1,17:5,26:8,22:1,4:3};
